0c13efd11d116d83f0f6de81d3bd82d9865856b2
twinkeee7 <167674604+twinkeee7@users.noreply.github.com>
2024-05-18 04:20:10 +0530

    Update config.py
    Signed-off-by: twinkeee7 <167674604+twinkeee7@users.noreply.github.com>

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ START_IMG = getenv("START_IMG", "https://telegra.ph/file/b64a1fa450c5842fe5b47.j
 SESSION = getenv("SESSION", None)
 
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Riseeuniverse")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Riseeuniverse")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/DevilCommunityyy")
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "7062539103").split()))
 
